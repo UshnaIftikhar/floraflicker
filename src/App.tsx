3f8e88532f78dfcb114abe6f397e9527babe508a
Ushna Iftikhar <crimepulse08@gmail.com>
2025-12-26 14:45:55 +0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Hero from './components/Hero';
+import Products from './components/Products';
+import WhyFloraFlicker from './components/WhyFloraFlicker';
+import GiftSection from './components/GiftSection';
+import Gallery from './components/Gallery';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-amber-50 via-rose-50 to-amber-50">
+      <Hero />
+      <Products />
+      <WhyFloraFlicker />
+      <GiftSection />
+      <Gallery />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

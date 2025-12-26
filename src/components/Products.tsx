25faffba0f3e56b984de981630a93ed4714f5159
Ushna Iftikhar <crimepulse08@gmail.com>
2025-12-26 20:33:37 +0500

    Update Products.tsx

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react';
 import ProductCard from './ProductCard';
 
-// Import images from assets
+// Import images from src/assets
 import BearCandle from './assets/bearcandle.jpg';
 import FlowerCandle from './assets/flowercandle.jpg';
 import ConeGlowCandle from './assets/coneglowcandle.jpg';

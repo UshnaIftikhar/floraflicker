0d71f5fbc0c1238d1da43e380818d6d173cf4837
Ushna Iftikhar <crimepulse08@gmail.com>
2025-12-28 11:38:31 +0500

    Create CartContext.tsx

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+export interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+}
+
+interface CartContextType {
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+  removeFromCart: (id: number) => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export function CartProvider({ children }: { children: ReactNode }) {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const addToCart = (item: CartItem) => {
+    setCart(prev => [...prev, item]);
+  };
+
+  const removeFromCart = (id: number) => {
+    setCart(prev => prev.filter(item => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used inside CartProvider');
+  }
+  return context;
+}

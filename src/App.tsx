1f65643a38ebcb75b55baef6bdaf2005ee38e415
Ushna Iftikhar <crimepulse08@gmail.com>
2025-12-29 15:04:27 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-
 import Hero from './components/Hero';
 import Products from './components/Products';
 import WhyFloraFlicker from './components/WhyFloraFlicker';
 import GiftSection from './components/GiftSection';
 import Footer from './components/Footer';
 import SideCart from './components/SideCart';
-import Checkout from './pages/Checkout';
 
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-amber-50 via-rose-50 to-amber-50">
-      <Routes>
-        {/* HOME PAGE */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <Products />
-              <WhyFloraFlicker />
-              <GiftSection />
-              <Footer />
-              <SideCart />
-            </>
-          }
-        />
-
-        {/* CHECKOUT PAGE */}
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
+      <Hero />
+      <Products />
+      <WhyFloraFlicker />
+      <GiftSection />
+      <Footer />
+      <SideCart /> {/* ✅ Slide-in side cart */}
     </div>
   );
 }

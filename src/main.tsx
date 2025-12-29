bb44256cdc242fba2b5461e759e0741ea1e1b943
Ushna Iftikhar <crimepulse08@gmail.com>
2025-12-29 15:03:25 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { CartProvider } from "./context/CartContext";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { CartProvider } from './context/CartContext';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <CartProvider>
       <App />
     </CartProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
